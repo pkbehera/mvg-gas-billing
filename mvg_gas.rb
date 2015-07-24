e19fd374139263d8dfadbb4ae037c875cc0e0a82
Prasanta Behera <pkbehera@lycos.com>
2015-07-24 19:20:50 +0530

    Update mvg_gas.rb
    Run it from the current directory so that there is less chances of providing wrong inputs

--- mvg_gas.rb
+++ mvg_gas.rb
@@ -1,17 +1,84 @@
 #!/usr/bin/ruby
-require 'flat'
+scriptDir = File.dirname(__FILE__)
+require "#{scriptDir}/flat"
+require "date"
 
+cwd = Dir.getwd
+workDir = File.basename(cwd)
+today = Date.today
+reqDir = today.strftime("%b%Y")
+curMon = today.strftime("%b")
+curYr = today.strftime("%Y")
+
+preMonth = Time.local(today.year, (today.month - 1), today.day)
+preMonthDir = preMonth.strftime("%b%Y")
+preMonth = preMonth.strftime("%b_%Y")
+
+if ! workDir.eql? reqDir then
+   puts "This script should be run from a folder named #{reqDir}"
+   abort
+end
+
+kycFile = "KYC.xls"
+if ! File.exist?(kycFile) then
+   puts "File #{kycFile} does not exist"
+   abort
+end
+
+occFile = "OCC.xls"
+if ! File.exist?(occFile) then
+   puts "File #{occFile} does not exist"
+   abort
+end
+
+rdngFilePat = "gasmeter_readings_{[0-3][0-9]}#{curMon}_to_{[0-3][0-9]}#{curMon}.xls"
+list = Dir.glob(rdngFilePat)
+if list.empty? then
+   puts "Current month readings file of pattern #{rdngFilePat} does not exist"
+   abort
+else
+   if list.size > 1 then
+      puts "There are more than one file of pattern #{rdngFilePat}"
+      abort
+   end
+   rdngFile = list[0]
+end
+
+ledgFile = "../#{preMonthDir}/Gas_Ledger_#{preMonth}.xls"
+if ! File.exist?(ledgFile) then
+   puts "File #{ledgFile} does not exist"
+   abort
+end
+
+if ARGV.length > 0 then
+   notesFile = "Notes_#{curMon}_#{curYr}.txt"
+   puts "Redirecting console output to #{notesFile}"
+   $stdout = File.new(notesFile, 'w')
+   $stdout.sync = true
+end
+
+puts "Current working directory #{cwd}"
+
+'''
 #NUM_ARGS = 5
 NUM_ARGS = 4
 if ARGV.length != NUM_ARGS then
    puts "Requires exactly #{NUM_ARGS} arguemnts"
-   #puts 'Usage: ' + $0 + ' KYC_excel_file' + ' Occupancy_excel_file' + ' latest_reading_excel_file' + ' gas_ledger_excel_file' + ' outstanding_excel_file'
-   puts 'Usage: ' + $0 + ' KYC_excel_file' + ' Occupancy_excel_file' + ' latest_reading_excel_file' + ' gas_ledger_excel_file'
+   #puts "Usage: #{$0} KYC_file Occupancy_file latest_reading_file gas_ledger_file outstanding_file"
+   puts "Usage: #{$0} KYC_file Occupancy_file latest_reading_file gas_ledger_file"
    abort
 end
-#if ARGV[0].split(".").last != 'xls' or ARGV[1].split(".").last != 'xls' or ARGV[2].split(".").last != 'xls' or ARGV[3].split(".").last != 'xls' or ARGV[4].split(".").last != 'xls' then
-if ARGV[0].split(".").last != 'xls' or ARGV[1].split(".").last != 'xls' or ARGV[2].split(".").last != 'xls' or ARGV[3].split(".").last != 'xls' then
-    puts 'Can not process file formats other than .xls (Excel 97-2003), save the files as .xls and retry.'
+
+kycFile = ARGV[0]
+occFile = ARGV[1]
+rdngFile = ARGV[2]
+ledgFile = ARGV[3]
+outsdFile = ARGV[4]
+'''
+
+#if kycFile.split(".").last != 'xls' or occFile.split(".").last != 'xls' or rdngFile.split(".").last != 'xls' or ledgFile.split(".").last != 'xls' or outsdFile.split(".").last != 'xls' then
+if kycFile.split(".").last != 'xls' or occFile.split(".").last != 'xls' or rdngFile.split(".").last != 'xls' or ledgFile.split(".").last != 'xls' then
+    puts "Can not process file formats other than .xls (Excel 97-2003), save the files as .xls and retry."
     abort
 end
 
@@ -34,12 +101,12 @@ if MVG_TOTAL_NUMBER_OF_FLATS != count then
 end
 
 #Read KYC excel file
-puts 'Reading file ' + ARGV[0]
-book = Spreadsheet.open ARGV[0]
+puts 'Reading file ' + kycFile
+book = Spreadsheet.open kycFile
 sheet = book.worksheet 0
 row = sheet.row(0)
 if row[KYC_BLOCK_COL_NO] != KYC_BLOCK_COL_HEADING or KYC_KYC_COL_HEADING != row[KYC_KYC_COL_NO] then
-    puts 'The format of the KYC Excel file ' + ARGV[0] + ' does not seems to be correct, check the file and retry!'
+    puts 'The format of the KYC Excel file ' + kycFile + ' does not seems to be correct, check the file and retry!'
     abort
 end
 num_flats = 0
@@ -66,12 +133,12 @@ if MVG_TOTAL_NUMBER_OF_FLATS != count || MVG_TOTAL_NUMBER_OF_FLATS != num_flats 
 end
 
 #Read Occupancy excel file
-puts 'Reading file ' + ARGV[1]
-book = Spreadsheet.open ARGV[1]
+puts 'Reading file ' + occFile
+book = Spreadsheet.open occFile
 sheet = book.worksheet 0
 row = sheet.row(0)
 if row[OCC_BLOCK_COL_NO] != OCC_BLOCK_COL_HEADING or OCC_OCC_COL_HEADING != row[OCC_OCC_COL_NO] then
-    puts 'The format of the Occupancy Excel file ' + ARGV[1] + ' does not seems to be correct, check the file and retry!'
+    puts 'The format of the Occupancy Excel file ' + occFile + ' does not seems to be correct, check the file and retry!'
     abort
 end
 num_flats = 0
@@ -95,13 +162,13 @@ if MVG_TOTAL_NUMBER_OF_FLATS != count || MVG_TOTAL_NUMBER_OF_FLATS != num_flats 
 end
 
 #Read latest gas reading excel file
-puts 'Reading file ' + ARGV[2]
+puts 'Reading file ' + rdngFile
 count = 0
-book = Spreadsheet.open ARGV[2]
+book = Spreadsheet.open rdngFile
 sheet = book.worksheet 0
 row = sheet.row(0)
 if row[READING_BLOCK_COL_NO] != READING_BLOCK_COL_HEADING or READING_CONS_COL_HEADING != row[READING_CONS_COL_NO] then
-    puts 'The format of the reading Excel file ' + ARGV[2] + ' does not seems to be correct, check the file and retry!'
+    puts 'The format of the reading Excel file ' + rdngFile + ' does not seems to be correct, check the file and retry!'
     abort
 end
 sheet.each do |row|
@@ -110,12 +177,21 @@ sheet.each do |row|
     block = row[READING_BLOCK_COL_NO]
     flat = row[READING_FLAT_COL_NO].to_i
     consumed = row[READING_CONS_COL_NO].to_f
+    negCount = 0;
     if consumed < 0 then
-        puts 'ERROR - NEGATIVE CONSUMPTION FOUND, THERE IS SOMETHING WRONG WITH THE READINGS. CORRECT THE READINGS AND RETRY!'
-        abort
+        puts "ERROR - NEGATIVE CONSUMPTION FOUND FOR #{block}-#{flat}!"
+        negCount = negCount + 1
     end
     if consumed > 25 then
         puts "WARNING!! HIGH consumtion, #{consumed} m^3 found for #{block}-#{flat}! Continuing bill generation, but review the readings of this flat!"
+    end
+    if negCount > 0 then
+        if ALLOW_NEGATIVE_READINGS != true then
+            puts 'NEGATIVE READINGS FOUND, THERE IS SOMETHING WRONG WITH THE READINGS. CORRECT THE READINGS AND RETRY'
+            abort
+        else
+            puts "Continuing bill generation, but review the readings of these flats!"
+        end
     end
     Flat.set_consumption(block, flat, consumed)
     count = count + 1
@@ -127,8 +203,8 @@ puts 'Checking data sanity...'
 Flat.check_sanity
 
 #Read gas ledger excel file
-puts 'Reading file ' + ARGV[3]
-book = Spreadsheet.open ARGV[3]
+puts 'Reading file ' + ledgFile
+book = Spreadsheet.open ledgFile
 sheet = book.worksheet 0
 row = sheet.row(0)
 if row[LEDGER_BLOCK_COL_NO] != LEDGER_BLOCK_COL_HEADING or row[LEDGER_FLAT_COL_NO] != LEDGER_FLAT_COL_HEADING or row[LEDGER_UNAC_DEBIT_COL_NO] != LEDGER_UNAC_DEBIT_COL_HEADING or row[LEDGER_NO_READING_COUNT_COL_NO] != LEDGER_NO_READING_COUNT_COL_HEADING then
@@ -156,8 +232,8 @@ end
 
 =begin
 #Read outstandings excel file
-puts 'Reading file ' + ARGV[4]
-book = Spreadsheet.open ARGV[4]
+puts 'Reading file ' + outsdFile
+book = Spreadsheet.open outsdFile
 sheet = book.worksheet 0
 row = sheet.row(1)
 if row[OUTSTAND_BLOCK_FLAT_COL_NO] != OUTSTAND_BLOCK_FLAT_COL_HEADING or row[OUTSTAND_BAL_COL_NO] != OUTSTAND_BAL_COL_HEADING then
